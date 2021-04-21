18993d3dda2ec85f21feefc8ff07c6f15d704b0a
goutamraj <76112854+goutamraj@users.noreply.github.com>
2021-04-21 12:40:07 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,42 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+
+float inp(float *arr,int *n)
+{
+    printf("Enter the no of elements you want to add : \n");
+    scanf("%d",n);
+
+    for(int i =0;i<*n;i++)
+    {   
+        printf("Element %d : ",(i+1));
+        scanf("%f",&arr[i]);
+    }
+    
+}
+
+float sum(float *arr,int *n)
+{
+	float sum;
+	for(int i=0;i<*n;i++)
+	{
+		sum+=*(arr + i);
+	}
+	return sum;
+}
+		
+
+void out(float total)
+{
+    printf("The sum of Numbers entered is %.3f",total);
+}
+
+int main()
+{
+    int *n;
+    float arr[100];
+    float tot;
+    inp(arr,n);
+    tot=sum(arr,n);
+    out(tot);
+    return 0;
+}
